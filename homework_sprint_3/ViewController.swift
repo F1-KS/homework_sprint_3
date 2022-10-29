dd374671570fde1b9c62fab585af9a6f8c7033b6
Dmitry Ivanov <f1-ks@mail.ru>
2022-10-29 22:00:26 +0300

    Initial Commit

---
+++ homework_sprint_3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  homework_sprint_3
+//
+//  Created by Dmitry Ivanov on 29.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
