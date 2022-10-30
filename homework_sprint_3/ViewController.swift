b59387387814e7b4f579c9f17420830773331acb
Dmitry Ivanov <f1-ks@mail.ru>
2022-10-30 08:09:40 +0300

    Домашнее задание
    Условия задачи
    Нужно создать приложение-счётчик. Его идея очень проста: в приложении должна быть одна большая кнопка. Сколько раз на неё нажали — такая цифра и отображается на счётчике.

--- homework_sprint_3/ViewController.swift
+++ homework_sprint_3/ViewController.swift
@@ -8,12 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var labelScore: UILabel!
+    @IBOutlet weak var countButton: UIButton!
+    private var score = 42 // Начальное значение счетчика 42, хотя по начальному условию требовалось начинать с 0
+
+       
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+        labelScore.text = "\(score)" // при запуске приложения устанавливаем начальное значение в Label, в нашем случае начальное значение переменной score (42)
+     }
 
+    @IBAction func countLabelButton(_ sender: Any) /*Описываем действие при нажатии кнопки*/{
+        print("О, кто-то нажал кнопку")
+        score += 1                      // при каждом нажатии на кнопку значение Label увеличивается на единицу
+        labelScore.text = "\(score)" // новое значение помещается в переменную score
+        }
 
 }
 
